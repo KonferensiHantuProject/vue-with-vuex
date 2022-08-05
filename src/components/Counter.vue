2b098099b529e4f47a22b0973bb4c778f756735f
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-08-05 11:11:22 +0700

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Penghitung dengn Vuex</h1>
+    <h1 class="counter">{{ $store.state.counter }}</h1>
+
+    <!-- Button Minus-->
+    <button>-</button>
+
+    <!-- Input -->
+    <input type="number" v-model="value">
+
+    <!-- Button Plus-->
+    <button>+</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: 1,
+        }
+    }
+}
+</script>
+
+<style>
+    .counter{
+        font-size: 5rem;
+    }
+
+    button{
+        border-radius: 100%;
+        border: none;
+        width: 2rem;
+        height: 2rem;
+        font-weight: 700;
+        cursor: pointer;
+    }
+
+    input{
+        padding: 0.4rem;
+        margin: 0 0.5rem;
+        text-align: center;
+    }
+</style>
